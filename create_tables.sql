1103584337abf97dceb40018194454386d09cbf2
Melanie Passos <71110484+MelanieP20@users.noreply.github.com>
2025-03-18 19:56:10 -0300

    Add files via upload

---
+++ create_tables.sql
@@ -0,0 +1,72 @@
+CREATE OR REPLACE TABLE t_payment (
+    payment_id INT PRIMARY KEY, -- unique payment indicator
+    order_id INT NOT NULL, --order identifier
+    user_id INT NOT NULL, --user identifier
+    payment_method VARCHAR(50) NOT NULL, --payment method for example: card, pix, boleto/factura, mercado pago
+    payment_status VARCHAR(50) NOT NULL, --payment status for example: refused, paid
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --payment date, timestamp based on user timezone
+    amount DECIMAL (10, 2) NOT NULL, --purchase amount
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES t_order(order_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES t_customer(user_id)
+);
+
+CREATE OR REPLACE TABLE t_customer(
+    user_id INT PRIMARY KEY, --unique user identifier, if the application does not have an unique identifier use AUTO_INCREMENTED
+    email VARCHAR (255) NOT NULL, --user email
+    first_name VARCHAR (100) NOT NULL, --user first name
+    last_name VARCHAR (100) NOT NULL, --user last name
+    user_address VARCHAR (255), --user address
+    gender VARCHAR (30), --user gender for example: male, female, non-binary, agender, prefere not to say, ...
+    birth_date DATE, --user birth date
+    phone_number VARCHAR (20), --user phone number, must use varchar for formating and phone numerbers starting with 0
+    country VARCHAR (15), -- user account country based
+    country_code VARCHAR(10) -- user account country code based
+
+);
+
+CREATE OR REPLACE TABLE t_order(
+    order_id INT PRIMARY KEY, --unique order identifier
+    user_id INT NOT NULL, --user identifier
+    item_id NUMBER NOT NULL, --item identifier
+    quantity INT NOT NULL, --item quantity sold
+    unite_price DECIMAL (10,2),--item unite price
+    total_price DECIMAL (10,2),--total price based on how much of the item has been sold (unite_price * quantity)
+    payment_id NUMBER NOT NULL, --payment identifier
+    order_status VARCHAR (50), --current order status for example: pending, completed, canceled
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --order generated date, timestamp based on user timezone
+    CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES t_item(item_id),
+    CONSTRAINT fk_payment FOREIGN KEY (payment_id) REFERENCES t_payment(payment_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES t_customer(user_id)
+);
+
+CREATE OR REPLACE TABLE t_category(
+    catg_id INT PRIMARY KEY, --unique category identifier
+    parent_catg_name VARCHAR(255), --parent category name
+    parent_catg_description TEXT, --parent detailed category description
+    child_catg_name VARCHAR(255), --child category name
+    child_catg_description TEXT, --child detailed category description    
+    catg_path VARCHAR (255), --hierarchy category path
+    parent_catg_id NUMBER, --parent category reference (NULL for parent)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP --creation category timestamp
+);
+
+CREATE OR REPLACE TABLE t_item(
+    item_id INT PRIMARY KEY, --unique item identifier
+    item_name VARCHAR(255), --item name
+    catg_id NUMBER NOT NULL, --associated category identifier
+    unite_price DECIMAL(10,2) NOT NULL, --unite price for item
+    item_description  TEXT, --detailed item description
+    item_status VARCHAR(50), --item status for example: active, deactive
+    termination_date DATE, --termination date from item
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --creation item timestamp,
+    CONSTRAINT fk_category FOREIGN KEY (catg_id) REFERENCES t_category(catg_id)
+);
+
+CREATE OR REPLACE TABLE t_item_status_history (
+    history_id INT PRIMARY KEY,  -- unique history id 
+    item_id INT NOT NULL, -- unique item identifier
+    price DECIMAL(10,2) NOT NULL, --item price in the register moment
+    status VARCHAR(50) NOT NULL, --item status for example: active, inactive
+    record_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,-- history item timestamp
+    CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES t_item(item_id)  
+);
---
+++ respuestas_negocio.sql
@@ -0,0 +1,157 @@
+
+CREATE OR REPLACE VIEW melanie_db.mlc.vw_birthday_customers_sales AS
+WITH customer_cte AS (
+    SELECT
+        user_id,
+        email,
+        first_name,
+        last_name,
+        gender,
+        birth_date
+    FROM melanie_db.mlc.t_customer
+    WHERE TO_CHAR(birth_date, 'MM-DD') = TO_CHAR(CURRENT_DATE(),'MM-DD')  
+),
+
+order_cte AS (
+    SELECT 
+        user_id,
+        SUM(total_price) AS total_order_price  
+    FROM melanie_db.mlc.t_order
+    WHERE TO_CHAR(order_date, 'YYYYMM') = '202001' AND order_status = 'completed'
+    GROUP BY user_id
+),
+
+payment_cte AS (
+    SELECT
+        user_id,
+        LISTAGG(DISTINCT payment_method, ', ') WITHIN GROUP (ORDER BY payment_method) AS payment_methods, 
+        SUM(amount) AS total_payment_amount 
+    FROM melanie_db.mlc.t_payment
+    WHERE payment_status = 'paid'
+    GROUP BY user_id
+),
+
+final_cte AS (
+    SELECT
+        c.user_id AS "customer id",
+        c.email AS "customer email",
+        c.first_name AS "customer first name",
+        c.last_name AS "customer last name",
+        c.gender AS "customer gender",
+        c.birth_date AS "birth date",
+        o.total_order_price AS "order final price",
+        p.payment_methods AS "payment methods",  
+        p.total_payment_amount AS "final payment amount"  
+    FROM customer_cte c
+    LEFT JOIN order_cte o ON c.user_id = o.user_id
+    LEFT JOIN payment_cte p ON c.user_id = p.user_id
+)
+
+SELECT * FROM final_cte;
+--------------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW melanie_db.mlc.vw_top5_cellphone_sellers AS
+WITH customer_cte AS (
+    SELECT
+        user_id,
+        first_name,
+        last_name
+    FROM melanie_db.mlc.t_customer
+),
+
+order_cte AS (
+    SELECT 
+        user_id,
+        item_id,  
+        order_status,
+        TO_CHAR(order_date, 'YYYY-MM') AS order_date_by_month, 
+        COUNT(order_id) AS order_count, 
+        SUM(quantity) AS total_quantity,  
+        SUM(total_price) AS total_order_price  
+    FROM melanie_db.mlc.t_order
+    WHERE TO_CHAR(order_date, 'YYYY') = '2020' AND order_status = 'completed'
+    GROUP BY user_id, item_id, order_status, order_date_by_month
+),
+
+payment_cte AS (
+    SELECT
+        user_id,
+        payment_id,
+        LISTAGG(payment_method, ',') WITHIN GROUP (ORDER BY payment_id) as payment_methods,  
+        TO_CHAR(payment_date, 'YYYY-MM') AS payment_date_by_month,  
+        payment_status,  
+        SUM(amount) AS total_payment_amount  
+    FROM melanie_db.mlc.t_payment p
+    WHERE p.payment_status = 'paid'
+    GROUP BY user_id, payment_id, payment_date_by_month, payment_status
+),
+
+item_cte AS (
+    SELECT 
+        item_id,
+        catg_id
+    FROM melanie_db.mlc.t_item
+),
+
+category_cte AS (
+    SELECT 
+        catg_id,
+        child_catg_name,
+        catg_path
+    FROM melanie_db.mlc.t_category t
+),
+
+final_cte AS (
+    SELECT
+        c.user_id AS "user id",
+        c.first_name AS "customer first name",
+        c.last_name AS "customer last name",
+        o.order_status AS "order status", 
+        o.order_date_by_month AS "sell date",  
+        o.order_count AS "sell amount",  
+        o.total_quantity AS "product quantity sold",  
+        o.total_order_price AS "total order price",
+        t.catg_id as "category id",  
+        t.child_catg_name as "category name",  
+        t.catg_path as "category path",  
+        o.item_id as "item_id",  
+        p.payment_methods AS "payment methods",  
+        p.payment_status AS "payment status",  
+        p.total_payment_amount AS "total payment amount",  
+        ROW_NUMBER() OVER (PARTITION BY o.order_date_by_month ORDER BY o.total_order_price DESC) AS rank
+    FROM customer_cte c
+    LEFT JOIN order_cte o ON c.user_id = o.user_id  
+    LEFT JOIN item_cte i ON o.item_id = i.item_id  
+    LEFT JOIN category_cte t ON i.catg_id = t.catg_id  
+    LEFT JOIN payment_cte p ON c.user_id = p.user_id AND o.order_date_by_month = p.payment_date_by_month 
+)
+
+SELECT * 
+FROM final_cte 
+WHERE rank <= 5  
+ORDER BY "sell date", rank;
+
+-------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE usp_item_status_history()
+  RETURNS STRING
+  LANGUAGE SQL
+  EXECUTE AS OWNER
+AS
+$$
+BEGIN
+  -- Inserir o histórico do item, baseado na tabela t_item
+  INSERT INTO t_item_status_history (item_id, price, status, record_date)
+  SELECT
+    item_id,
+    unite_price AS price,
+    item_status AS status,
+    CURRENT_TIMESTAMP AS record_date
+  FROM
+    melanie_db.mlc.t_item
+  WHERE
+    -- Verifica se o item foi atualizado desde a última vez
+    created_at > (SELECT MAX(record_date) FROM t_item_status_history WHERE item_id = t_item.item_id);
+
+  RETURN 'Histórico inserido com sucesso';
+END;
+$$;
